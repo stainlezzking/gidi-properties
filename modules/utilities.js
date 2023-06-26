5260923ac1b52ecf594bb6160c06a8ade7d683f7
Chukwuebuka Stanley <azukachukwuebuka07@gmail.com>
2023-06-26 16:32:46 +0100

    getpaginated codes from utility file, it was causing circular dependency error

--- modules/utilities.js
+++ modules/utilities.js
@@ -1,4 +1,4 @@
-const { SITE, APARTMENTS } = require("./db")
+// const { SITE, APARTMENTS } = require("./db")
 
 // localgovs = [
 //     {localgov : "Alimosho", hide : true, group : []},
@@ -35,8 +35,6 @@ const { SITE, APARTMENTS } = require("./db")
 // ]
 
 
-
-
 module.exports.amenities = [
     {
         name : "prepaid Meter",
@@ -64,53 +62,11 @@ module.exports.amenities = [
     },
 ]
 
-// Pagination function
-module.exports.getPaginatedData = async function(pageNumber, pageSize, next, query = {}) {
-    try {
-      const pipeline = [
-      { $match: { complete : true, approved : true, ...query } },
-      { $count: 'count' },
-    ];
-  
-    
-   
-    const [result] = await APARTMENTS.aggregate(pipeline);
-    const totalCount = result ? result.count : 0;
-
-    const paginatedData = await APARTMENTS.aggregate([
-      { $match: { complete : true, approved : true, ...query  } },
-      { $sort: { createdAt: -1 } },
-      { $skip: (pageNumber - 1) * pageSize },
-      { $limit: pageSize },
-      {$project : {carousel : 1, _id : 1, proptype : 1, cost : 1, bathrooms : 1, rooms : 1, area : 1, label : 1, localgovs : 1, complete : 1}}
-    ]);
-    
-    return {
-      paginatedData,
-      pagin : {pageList : new Array(Math.ceil(totalCount/pageSize)), pageNumber, pageSize}
-    };
-
-    } catch (error) {
-      return next(error)
-    }
-  }
-
-  module.exports.showPaginatedList = function(currentIndex, totalpages){
-        let startpoint;
-        currentIndex > 5 ? startpoint = (currentIndex-5) : startpoint = currentIndex- (currentIndex-1)
-
-        let endpoint;
-        totalpages.length <= currentIndex+5 ? endpoint = totalpages.length : (currentIndex <= 5 && totalpages.length > currentIndex+5) ? endpoint = (startpoint + 9) : endpoint = currentIndex + 5;
-
-        let pagination = []
-        for(i = startpoint; i<= endpoint; i++){
-        pagination.push(i);
-        }
-        return {list : pagination, currentIndex, totalpages};
-}
   
 module.exports.contactsSelect = ['Landlord', 'Care-taker', 'Tenant']
 
+// do not change the arrangement of this propsselection 
+// as it is being accessed throughout the app with its index.
 module.exports.propsSelection = ['Self-contain', 'Apartment'] 
 
 module.exports.listingTypes = [{label : "All listings", code : "listings"},{label : 'Apartments', code : 'apt'}, {label : 'Self-con', code : 'sfc'}]
